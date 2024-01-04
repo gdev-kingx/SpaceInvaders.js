40bc1851cdfea469eb1a4d03306101cf134fc315
R Sarveshvarn <151063233+gdev-kingx@users.noreply.github.com>
2024-01-05 00:30:34 +0530

    Add files via upload

--- js/classes/Particle.js
+++ js/classes/Particle.js
@@ -1,23 +1,30 @@
-class Projectile {
-    constructor({ position, velocity, color = 'red' }) {
+class Particle {
+    constructor({ position, velocity, radius, color, fades }) {
         this.position = position
         this.velocity = velocity
 
-        this.radius = 4
+        this.radius = radius
         this.color = color
+        this.opacity = 1
+        this.fades = fades
     }
 
     draw() {
+        c.save()
+        c.globalAlpha = this.opacity
         c.beginPath()
         c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
         c.fillStyle = this.color
         c.fill()
         c.closePath()
+        c.restore()
     }
 
     update() {
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
+        if (this.fades) this.opacity -= 0.01
     }
 }
